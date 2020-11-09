ea044255fbf8559659ac940c0e87863d37decc3c
mikata <h-mikata@brain-d.jp>
2020-11-09 14:26:40 +0900

    add insert, update

--- app1/views/about_view.py
+++ app1/views/about_view.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from app1.models.topic import Topic
 
 
 class AboutView(TemplateView):
@@ -7,6 +8,16 @@ class AboutView(TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx['message'] = 'this is get.'
+        ctx['all'] = Topic.objects.all()
+        #where (hairetu de toreru)
+        ctx['id2'] = Topic.objects.filter(id=2)
+        getid = Topic.objects.get(id=2)
+        getid.value=3
+        #update set
+        getid.save()
+        ctx['getid'] = getid
+        #insert into 
+        Topic(value=1, question_text='question_text', pub_date='2020-11-10 00:00:00.000000').save()
         return ctx
     def post(self, request, *args, **kwargs):
         ctxt= {
